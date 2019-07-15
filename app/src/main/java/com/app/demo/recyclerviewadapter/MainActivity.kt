2b962dc3f8f2bf0611458241a70b40e9b5548518
hieup <hieupham1993@live.com>
2019-07-15 22:00:04 +0700

    Update 1.0.1

--- app/src/main/java/com/app/demo/recyclerviewadapter/MainActivity.kt
+++ app/src/main/java/com/app/demo/recyclerviewadapter/MainActivity.kt
@@ -4,8 +4,10 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.abs
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -18,34 +20,163 @@ class MainActivity : AppCompatActivity() {
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = ColorAdapter(
-                    listOf(Color.parseColor("#ff0f0f"),
-                            Color.parseColor("#0fff0f"),
-                            Color.parseColor("#f3e11f"),
-                            Color.parseColor("#ab3ec1"))
+                    listOf(
+                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+                    )
             ) { _, item ->
                 Toast.makeText(context, item.toString(), Toast.LENGTH_SHORT).show()
             }.also {
                 colorAdapter = it
             }
         }
-        button.setOnClickListener {
-            colorAdapter.addItem(
-                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
-                    true
+        spinner.apply {
+            adapter = ArrayAdapter(
+                    context, android.R.layout.simple_list_item_1,
+                    mutableListOf(
+                            "Clear data",
+                            "Add more",
+                            "4 More",
+                            "Remove first",
+                            "Remove random",
+                            "Remove 2 start 4",
+                            "Remove all color > 5.000k",
+                            "Remove all color < 5.000k",
+                            "Retain all color > 5.000k",
+                            "Retain all color < 5.000k",
+                            "Insert to position 2",
+                            "Insert 3 to position 1",
+                            "Move 4 to position 1",
+                            "Reverse",
+                            "Shuffle",
+                            "Update position 2",
+                            "New list 5 items",
+                            "Sort ascending",
+                            "Sort descending",
+                            "Sort with Comparator"
+                    )
             )
-            recyclerView.scrollBy(0, Int.MAX_VALUE)
         }
-        button2.setOnClickListener {
-            colorAdapter.addItems(
-                    listOf(
+        button.setOnClickListener {
+            when (spinner.selectedItemPosition) {
+                0 -> {
+                    colorAdapter.clearData()
+                }
+                1 -> {
+                    colorAdapter.addItem(
                             Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                            true
+                    )
+                    recyclerView.scrollBy(0, Int.MAX_VALUE)
+                }
+                2 -> {
+                    colorAdapter.addItems(
+                            listOf(
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+                            ),
+                            true
+                    )
+                    recyclerView.scrollBy(0, Int.MAX_VALUE)
+                }
+                3 -> {
+                    colorAdapter.removeItemAt(0)
+                    recyclerView.scrollToPosition(0)
+                }
+                4 -> {
+                    try {
+                        colorAdapter.removeItem(colorAdapter.items.random())
+                    } catch (e: NoSuchElementException) {
+                        Toast.makeText(this, "Collection is empty", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                5 -> {
+                    colorAdapter.removeItems(3, 2)
+                }
+                6 -> {
+                    colorAdapter.removeAll {
+                        abs(it) > 5000000
+                    }
+                }
+                7 -> {
+                    colorAdapter.removeAll {
+                        abs(it) < 5000000
+                    }
+                }
+                8 -> {
+                    colorAdapter.retainAll {
+                        abs(it) > 5000000
+                    }
+                }
+                9 -> {
+                    colorAdapter.retainAll {
+                        abs(it) < 5000000
+                    }
+                }
+                10 -> {
+                    colorAdapter.insertItem(
                             Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
-                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
-                            Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
-                    ),
-                    true
-            )
-            recyclerView.scrollBy(0, Int.MAX_VALUE)
+                            2
+                    )
+                    recyclerView.scrollToPosition(2)
+                }
+                11 -> {
+                    colorAdapter.insertItem(
+                            listOf(
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+                            ),
+                            1
+                    )
+                    recyclerView.scrollToPosition(1)
+                }
+                12 -> {
+                    colorAdapter.moveItemTo(4, 1)
+                    recyclerView.scrollToPosition(1)
+                }
+                13 -> {
+                    colorAdapter.reverse()
+                }
+                14 -> {
+                    colorAdapter.shuffle()
+                }
+                15 -> {
+                    colorAdapter.updateItem(2, Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
+                    recyclerView.scrollToPosition(2)
+                }
+                16 -> {
+                    colorAdapter.updateItems(
+                            listOf(
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+                                    Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+                            )
+                    )
+                }
+                17 -> {
+                    colorAdapter.sortBy {
+                        it
+                    }
+                }
+                18 -> {
+                    colorAdapter.sortBy(ascending = false) {
+                        it
+                    }
+                }
+                19 -> {
+                    colorAdapter.sortWith(Comparator { t1, t2 ->
+                        t1 - t2
+                    })
+                }
+            }
         }
     }
 }
--- recyclerviewadapter/src/main/java/com/hieupt/recyclerviewadapter/RecyclerListAdapter.kt
+++ recyclerviewadapter/src/main/java/com/hieupt/recyclerviewadapter/RecyclerListAdapter.kt
@@ -1,13 +1,15 @@
 package com.hieupt.recyclerviewadapter
 
+import android.support.annotation.IntRange
 import java.util.*
+import kotlin.math.min
 
 /**
  * Created by HieuPT on 3/26/2019.
  */
 abstract class RecyclerListAdapter<VH : BaseRecyclerAdapter.BaseViewHolder<T>, T>(
-    items: List<T>? = null,
-    onItemClicked: OnItemClicked<T>? = null
+        items: List<T>? = null,
+        onItemClicked: OnItemClicked<T>? = null
 ) : BaseRecyclerAdapter<VH, T>(onItemClicked) {
 
     private val _items: MutableList<T> = items?.let { ArrayList(it) } ?: mutableListOf()
@@ -23,7 +25,18 @@ abstract class RecyclerListAdapter<VH : BaseRecyclerAdapter.BaseViewHolder<T>, T
         }
     }
 
-    fun addItem(item: T, notifyNow: Boolean = false) {
+    fun updateItem(@IntRange(from = 0) position: Int, item: T, notifyNow: Boolean = true) {
+        this._items.apply {
+            if (position in 0 until size) {
+                this[position] = item
+                if (notifyNow) {
+                    notifyItemChanged(position)
+                }
+            }
+        }
+    }
+
+    fun addItem(item: T, notifyNow: Boolean = true) {
         this._items.apply {
             add(item)
             if (notifyNow) {
@@ -32,7 +45,7 @@ abstract class RecyclerListAdapter<VH : BaseRecyclerAdapter.BaseViewHolder<T>, T
         }
     }
 
-    fun addItems(items: List<T>, notifyNow: Boolean = false) {
+    fun addItems(items: List<T>, notifyNow: Boolean = true) {
         this._items.apply {
             val currentSize = size
             addAll(items)
@@ -42,7 +55,150 @@ abstract class RecyclerListAdapter<VH : BaseRecyclerAdapter.BaseViewHolder<T>, T
         }
     }
 
-    fun clearData(notifyNow: Boolean = false) {
+    fun insertItem(item: T, @IntRange(from = 0) position: Int, notifyNow: Boolean = true) {
+        this._items.apply {
+            val index = min(position, size)
+            if (index == size) {
+                this.add(item)
+            } else {
+                this.add(index, item)
+            }
+            if (notifyNow) {
+                notifyItemInserted(index)
+            }
+        }
+    }
+
+    fun insertItem(items: List<T>, @IntRange(from = 0) position: Int, notifyNow: Boolean = true) {
+        this._items.apply {
+            val index = min(position, size)
+            if (index == size) {
+                this.addAll(items)
+            } else {
+                this.addAll(index, items)
+            }
+            if (notifyNow) {
+                notifyItemRangeInserted(index, items.size)
+            }
+        }
+    }
+
+    fun removeItem(item: T, notifyNow: Boolean = true) {
+        this._items.apply {
+            val position = indexOf(item)
+            if (position != -1) {
+                removeAt(position)
+                if (notifyNow) {
+                    notifyItemRemoved(position)
+                }
+            }
+        }
+    }
+
+    fun removeItems(@IntRange(from = 0) startPosition: Int, @IntRange(from = 1) itemCount: Int, notifyNow: Boolean = true) {
+        this._items.apply {
+            if (startPosition in 0 until size) {
+                val actualCount = if (startPosition + itemCount >= size) {
+                    size - startPosition
+                } else {
+                    itemCount
+                }
+                repeat(actualCount) {
+                    this.removeAt(startPosition)
+                }
+                if (notifyNow) {
+                    notifyItemRangeRemoved(startPosition, actualCount)
+                }
+            }
+        }
+    }
+
+    fun removeItemAt(@IntRange(from = 0) position: Int, notifyNow: Boolean = true) {
+        this._items.apply {
+            if (position in 0 until size) {
+                removeAt(position)
+                if (notifyNow) {
+                    notifyItemRemoved(position)
+                }
+            }
+        }
+    }
+
+    fun removeAll(notifyNow: Boolean = true, predicate: (T) -> Boolean) {
+        this._items.apply {
+            this.removeAll(predicate)
+            if (notifyNow) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    fun retainAll(notifyNow: Boolean = true, predicate: (T) -> Boolean) {
+        this._items.apply {
+            this.retainAll(predicate)
+            if (notifyNow) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    fun moveItemTo(@IntRange(from = 0) fromPosition: Int, @IntRange(from = 0) toPosition: Int, notifyNow: Boolean = true) {
+        this._items.apply {
+            if (fromPosition in 0 until size && toPosition in 0 until size && fromPosition != toPosition) {
+                val item = this.removeAt(fromPosition)
+                this.add(toPosition, item)
+                if (notifyNow) {
+                    notifyItemMoved(fromPosition, toPosition)
+                }
+            }
+        }
+    }
+
+    fun reverse(notifyNow: Boolean = true) {
+        this._items.apply {
+            this.reverse()
+            if (notifyNow) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    fun <R : Comparable<R>> sortBy(ascending: Boolean = true, notifyNow: Boolean = true, selector: (T) -> R?) {
+        this._items.apply {
+            if (ascending) {
+                this.sortBy(selector)
+            } else {
+                this.sortByDescending(selector)
+            }
+            if (notifyNow) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    fun sortWith(comparator: Comparator<in T>, notifyNow: Boolean = true) {
+        this._items.apply {
+            this.sortWith(comparator)
+            if (notifyNow) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    fun shuffle(random: Random? = null, notifyNow: Boolean = true) {
+        this._items.apply {
+            if (random != null) {
+                this.shuffle(random)
+            } else {
+                this.shuffle()
+            }
+            if (notifyNow) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    fun clearData(notifyNow: Boolean = true) {
         this._items.apply {
             clear()
             if (notifyNow) {
